cb522e96f14047b26d35fd4c6f67bb52c76582fe
mconbere <mconbere@gmail.com>
2012-12-03 11:23:17 -0800

    Update week1/sort.h
    Add a really simple port of wikipedia's comb sort. Test later, mostly just wanted to remember the name of the sorting algorithm...

--- week1/sort.h
+++ week1/sort.h
@@ -7,8 +7,30 @@
 
 namespace rtl {
 
-template <typename Iter, typename Comp>
-void mergesort(Iter first, Iter last, Comp comp);
+// Comb Sort implementation very closely based on Wikipedia's in-place pseudocode.
+// Comb Sort is an efficient variation of Bubble Sort.
+// Untested!
+template <typename Iter, typename Comp = std::less<typename std::iterator_traits<Iter>::value_type>>
+void combsort(Iter first, Iter last, Comp comp = Comp()) {
+  using std::iterator_traits<Iter>::size_type;
+
+  if (first >= last - 1) return;
+
+  size_type gap = (last - first);
+  bool swapped = false;
+
+  while ((gap > 1) || swapped) {
+    if (gap > 1) gap = static_cast<size_type>(static_cast<double>(gap) / 1.247330950103979);
+    swapped = false;
+    
+    for (Iter it = first; it + gap < last; ++it) {
+      if (comp(*(it + gap), *it)) {
+        std::iter_swap(it, it + gap);
+        swapped = true;
+      }
+    }
+  }
+}
 
 // Quicksort implementation very closely based on Wikipedia's in-place quicksort.
 // The determination of the pivot leaves much to be desired. 
